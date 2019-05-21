82bf8268b274a429f9dc459c73342b341033ebf9
Ryan Fitzgerald Releford <45986349+Front-End-Releford@users.noreply.github.com>
2019-05-21 11:24:06 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTest1 = New System.Windows.Forms.TextBox()
+        Me.txtTest2 = New System.Windows.Forms.TextBox()
+        Me.txtTest3 = New System.Windows.Forms.TextBox()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.lblGrade = New System.Windows.Forms.Label()
+        Me.btnCalcAvg = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Test1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Test2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Test3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 150)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Average"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 200)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Grade"
+        '
+        'txtTest1
+        '
+        Me.txtTest1.Location = New System.Drawing.Point(81, 28)
+        Me.txtTest1.Name = "txtTest1"
+        Me.txtTest1.Size = New System.Drawing.Size(100, 20)
+        Me.txtTest1.TabIndex = 5
+        Me.txtTest1.Text = " "
+        '
+        'txtTest2
+        '
+        Me.txtTest2.Location = New System.Drawing.Point(81, 62)
+        Me.txtTest2.Name = "txtTest2"
+        Me.txtTest2.Size = New System.Drawing.Size(100, 20)
+        Me.txtTest2.TabIndex = 6
+        '
+        'txtTest3
+        '
+        Me.txtTest3.Location = New System.Drawing.Point(81, 88)
+        Me.txtTest3.Name = "txtTest3"
+        Me.txtTest3.Size = New System.Drawing.Size(100, 20)
+        Me.txtTest3.TabIndex = 7
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(142, 150)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(39, 13)
+        Me.lblAverage.TabIndex = 8
+        '
+        'lblGrade
+        '
+        Me.lblGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGrade.Location = New System.Drawing.Point(142, 200)
+        Me.lblGrade.Name = "lblGrade"
+        Me.lblGrade.Size = New System.Drawing.Size(39, 13)
+        Me.lblGrade.TabIndex = 9
+        '
+        'btnCalcAvg
+        '
+        Me.btnCalcAvg.Location = New System.Drawing.Point(15, 247)
+        Me.btnCalcAvg.Name = "btnCalcAvg"
+        Me.btnCalcAvg.Size = New System.Drawing.Size(92, 40)
+        Me.btnCalcAvg.TabIndex = 10
+        Me.btnCalcAvg.Text = "Calculate Average"
+        Me.btnCalcAvg.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(145, 247)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 40)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalcAvg)
+        Me.Controls.Add(Me.lblGrade)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.txtTest3)
+        Me.Controls.Add(Me.txtTest2)
+        Me.Controls.Add(Me.txtTest1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Ryan-Releford-Grader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTest1 As TextBox
+    Friend WithEvents txtTest2 As TextBox
+    Friend WithEvents txtTest3 As TextBox
+    Friend WithEvents lblAverage As Label
+    Friend WithEvents lblGrade As Label
+    Friend WithEvents btnCalcAvg As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcAvg.Click
+
+        Dim test1, test2, test3, total As Integer
+        Dim average As Double
+
+        test1 = Convert.ToInt16(txtTest1.Text)
+        test2 = Convert.ToInt16(txtTest2.Text)
+        test3 = Convert.ToInt16(txtTest3.Text)
+
+        total = test1 + test2 + test3
+        average = total / 3.0
+
+        lblAverage.Text = average.ToString("####0.00")
+
+        If average >= 90 And average <= 100 Then
+            lblGrade.Text = "A"
+        ElseIf average >= 80 And average < 90 Then
+            lblGrade.Text = "B"
+        ElseIf average >= 70 And average < 80 Then
+            lblGrade.Text = "C"
+        ElseIf average >= 60 And average < 70 Then
+            lblGrade.Text = "D"
+        Else
+            lblGrade.Text = "F"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtTest1.Text = String.Empty
+        txtTest3.Text = String.Empty
+        txtTest2.Text = String.Empty
+        lblGrade.Text = String.Empty
+        lblAverage.Text = String.Empty
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles lblAverage.Click
+
+    End Sub
+
+    Private Sub TxtTest1_TextChanged(sender As Object, e As EventArgs) Handles txtTest1.TextChanged
+
+    End Sub
+
+    Private Sub TxtTest2_TextChanged(sender As Object, e As EventArgs) Handles txtTest2.TextChanged
+
+    End Sub
+
+    Private Sub TxtTest3_TextChanged(sender As Object, e As EventArgs) Handles txtTest3.TextChanged
+
+    End Sub
+End Class
